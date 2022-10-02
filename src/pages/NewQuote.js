d9ba0581c4341d7f9a30a2150e271d84b0b71d5b
Nawfaldev19 <108766715+Nawfaldev19@users.noreply.github.com>
2022-10-02 20:44:25 +0500

    Add files via upload

---
+++ src/pages/NewQuote.js
@@ -0,0 +1,14 @@
+import { useHistory } from "react-router-dom";
+import QuoteForm from "../components/quotes/QuoteForm";
+
+const NewQuote = () => {
+  const history = useHistory();
+  const AddQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+    history.push("/quotes");
+  };
+
+  return <QuoteForm onAddQuote={AddQuoteHandler} />;
+};
+
+export default NewQuote;
---
+++ src/pages/QuoteDetail.js
@@ -0,0 +1,39 @@
+import { Fragment } from "react";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
+
+import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "max", text: "heh" },
+  { id: "q2", author: "maxillion", text: "haha" },
+];
+
+const QuoteDetail = () => {
+  const params = useParams();
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+  const match = useRouteMatch();
+  console.log(match);
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
+  return (
+    <Fragment>
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`${match.path}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`${match.path}/comments`}>
+        <Comments />
+      </Route>
+    </Fragment>
+  );
+};
+
+export default QuoteDetail;
